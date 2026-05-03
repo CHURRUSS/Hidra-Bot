4875f7dbf5224919916c3f7b4c3a96d2fe12f7c6
CHURRUSS <mh34538@gmail.com>
2026-05-03 18:37:21 -0300

    Add files via upload

---
+++ offline.js
@@ -0,0 +1,23 @@
+﻿{
+	"version": 1777843878,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-3.4.1.min.js",
+		"offlineClient.js",
+		"images/nucleohidra-sheet0.png",
+		"images/nucleohidra-sheet1.png",
+		"images/enxamehidra.png",
+		"images/luas-sheet0.png",
+		"images/subnucleoshidra-sheet0.png",
+		"images/background-sheet0.png",
+		"images/iconaddmucisas-sheet0.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png",
+		"config.json"
+	]
+}
